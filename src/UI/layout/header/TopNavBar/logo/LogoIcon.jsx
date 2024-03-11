79a029631925ca9875035f9137f416e4e2f47105
SamahOud <you@example.com>
2024-03-11 21:29:58 +0200

    change

--- src/UI/layout/header/TopNavBar/logo/LogoIcon.jsx
+++ src/UI/layout/header/TopNavBar/logo/LogoIcon.jsx
@@ -1,14 +1,19 @@
 import { Avatar } from "@mui/material"
 import IconButton from '@mui/material/IconButton'
+import NavBarLink from "../../../../components/routes/NavBarLink"
+import ROUTES from "../../../../../core/routes/routesModel"
 
 const LogoIcon = () => {
     return (
-        <IconButton 
-            sx={{display: { xs: "inline-flex", md: "none" },
-                size: "large", edge: "start", color: "inherit",}}
-            >
-            <Avatar alt='me' src='/React-Cards/assets/images/business-card.png'></Avatar>
-        </IconButton>
+        <NavBarLink to={ROUTES.CARDS} color="white">
+            <IconButton 
+                sx={{display: { xs: "inline-flex", md: "none" },
+                    size: "large", edge: "start", color: "inherit",}}
+                >
+                <Avatar alt='me' src='/React-Cards/assets/images/business-card.png'></Avatar>
+            </IconButton>
+        </NavBarLink>
+        
     )
 }
 
